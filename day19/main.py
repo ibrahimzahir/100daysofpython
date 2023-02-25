98cfbc3333cb9498695964ceaae2aa52fcbf00fc
Ibrahim Zahir <74900384+ibrahimzahir@users.noreply.github.com>
2023-02-25 10:52:29 +0500

    Day19

---
+++ day19/main.py
@@ -0,0 +1,15 @@
+#List objects have a sort() method that will sort the list alphanumerically, ascending, by default:
+
+sortList = ["orange", "kunnanru", "mango", "kiwi", "pineapple", "banana"]
+sortList.sort()
+
+print(sortList)
+
+
+thislist = [402, 12, 100, 50, 65, 89, 10000, 82, 23]
+thislist.sort()
+print(thislist)
+
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort(reverse = True)
+print(thislist)
